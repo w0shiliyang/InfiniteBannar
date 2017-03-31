2d7c7622636f79a817d1ff1b4b9fa4ed2fddb708
liyang <13363182679@163.com>
2017-03-31 11:34:39 -0700

    支持xib、storyboard、纯代码

--- 无限滑动轮播图/LYBannarView.h
+++ 无限滑动轮播图/LYBannarView.h
@@ -10,24 +10,34 @@
 #import "XXNibBridge.h"
 
 @protocol LYBannarDelegate <NSObject>
+
+/**
+ 点击后的回调
+
+ @param index 第几个
+ @param model 数据回调
+ */
 -(void)bannarClickWithIndex:(NSUInteger)index model:(id)model;
 
 @end
 
 @interface LYBannarView : UIView<XXNibBridge>
 @property (weak,   nonatomic) id<LYBannarDelegate> delegate;
+
+/**
+ 传数据
+
+ @param dataArray 数组里是字符串
+ */
 -(void)setData:(NSArray *)dataArray;
+
+/**
+ 暂停自动轮播
+ */
 -(void)stopTimer;
+
+/**
+ 开始自动轮播
+ */
 -(void)startTimer;
 @end
-
-/*
- 
- for (NSInteger i = 0; i < _modelArray.count; i ++) {
- MKJItemModel *model = [[MKJItemModel alloc] init];
- model.imageName = [NSString stringWithFormat:@"%zd",i];
- model.titleName = [NSString stringWithFormat:@"原来第%zd张",i];
- [_datas addObject:model];
- }
- 
- */
--- 无限滑动轮播图/ViewController.m
+++ 无限滑动轮播图/ViewController.m
@@ -21,7 +21,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = RGB(97, 183, 247, 1);
+    
+#pragma mark - xib创建
     //多个轮播
+//    NSMutableArray * arr = [NSMutableArray new];
+//    for (NSInteger i = 0; i < 11; i ++) {
+//        LYBannarItemModel *model = [[LYBannarItemModel alloc] init];
+//        model.imageName = [NSString stringWithFormat:@"%zd",i];
+//        [arr addObject:model];
+//    }
+//    //单个不轮播
+//    /*
+//        LYBannarItemModel *model = [[LYBannarItemModel alloc] init];
+//        model.imageName = [NSString stringWithFormat:@"1zd"];
+//        [arr addObject:model];
+//     */
+//    //传递数据
+//    [_banarView setData:arr];
+//    //设置代理
+//    _banarView.delegate = self;
+    
+    
+#pragma mark - 纯代码创建
+    self.banarView.hidden = YES;
     NSMutableArray * arr = [NSMutableArray new];
     for (NSInteger i = 0; i < 11; i ++) {
         LYBannarItemModel *model = [[LYBannarItemModel alloc] init];
@@ -30,12 +52,18 @@
     }
     //单个不轮播
     /*
-        LYBannarItemModel *model = [[LYBannarItemModel alloc] init];
-        model.imageName = [NSString stringWithFormat:@"1zd"];
-        [arr addObject:model];
+     LYBannarItemModel *model = [[LYBannarItemModel alloc] init];
+     model.imageName = [NSString stringWithFormat:@"1zd"];
+     [arr addObject:model];
      */
+    self.banarView = [[NSBundle mainBundle]loadNibNamed:@"LYBannarView" owner:nil options:nil].lastObject;
+    self.banarView.frame = CGRectMake(0, 100, SCREEN_WIDTH, 200);
     [_banarView setData:arr];
+    //传递数据
     _banarView.delegate = self;
+    //设置代理
+    [self.view addSubview:self.banarView];
+    
     
 }
 
@@ -43,6 +71,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - LYBannarDelegate
 
 -(void)bannarClickWithIndex:(NSUInteger)index model:(id)model{
     NSLog(@"点击了第%ld个，数据是:%@",index,model);
